c19b1d710e788d4863df94d6b36c2e9e40f1188f
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2023-12-07 16:07:24 +0530

    first commit

---
+++ portfolio2.js
@@ -0,0 +1,25 @@
+const imageContainer = document.querySelector('.image-container');
+const myImage = document.querySelector('.image-adjustment');
+const border = document.querySelector('.my-image')
+// var col1 = '#0E1226'
+// var col2 = '#251B4F'
+// var col3 = '#fff'
+// var col4 = '#000'
+
+// function changecolor(){
+//         if( myImage.style.width === '200px' & myImage.style.height === '200px'){
+//             myImage.style.width = '220px';
+//             myImage.style.height = '220px';
+//             border.style.width = '220px'
+//         }
+//         else{
+//             myImage.style.width = '200px';
+//             myImage.style.height = '200px';
+//             border.style.width = '200px'
+//         }
+// }
+// changecolor()
+
+// setInterval(changecolor,1000)
+
+ 
