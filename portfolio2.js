b48cfacafc6a3d4216f17bda68f04df3bf81ccb6
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2023-12-07 16:51:14 +0530

    added a small animation for selfie image

--- portfolio2.js
+++ portfolio2.js
@@ -6,20 +6,22 @@ const border = document.querySelector('.my-image')
 // var col3 = '#fff'
 // var col4 = '#000'
 
-// function changecolor(){
-//         if( myImage.style.width === '200px' & myImage.style.height === '200px'){
-//             myImage.style.width = '220px';
-//             myImage.style.height = '220px';
-//             border.style.width = '220px'
-//         }
-//         else{
-//             myImage.style.width = '200px';
-//             myImage.style.height = '200px';
-//             border.style.width = '200px'
-//         }
-// }
-// changecolor()
+function changecolor(){
+        if( myImage.style.width === '200px' & myImage.style.height === '200px'){
+            myImage.style.width = '220px';
+            myImage.style.height = '220px';
+            border.style.width = '220px';
+            border.style.height = '220px'
+        }
+        else{
+            myImage.style.width = '200px';
+            myImage.style.height = '200px';
+            border.style.width = '200px';
+            border.style.height = '200px'
+        }
+}
+changecolor()
 
-// setInterval(changecolor,1000)
+setInterval(changecolor,1000)
 
  
