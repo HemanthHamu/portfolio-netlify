5655d633c4aa8ad1cdc93a3bd29580f7ce01fc9a
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2024-01-14 16:54:52 +0530

    new files added

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+  // function toggleMobileLinks() {
+  //   var mobileLinks = document.querySelector('.mobile-links');
+  //   mobileLinks.style.display = (mobileLinks.style.display === 'flex') ? 'none' : 'flex';
+  // }
+
+
+  function toggleMobileLinks() {
+    var mobileLinks = document.querySelector('.mobile-links');
+    mobileLinks.style.display = (mobileLinks.style.display === 'flex') ? 'none' : 'flex';
+}
+
+// Add an event listener to each navbar link
+var navbarLinks = document.querySelectorAll('.mobile-links a');
+navbarLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        var mobileLinks = document.querySelector('.mobile-links');
+        mobileLinks.style.display = 'none'; // Hide mobile links when a link is clicked
+    });
+});
